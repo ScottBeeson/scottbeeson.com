b5565bea89ebc47e65d0a30aff931a94a69f6f2e
Scott <sbeeson@gmail.com>
2016-04-28 18:45:52 -0500

    Transitions working

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 /**
  * Created by sbeeson@gmail.com on 2016-04-19.
  */
+
 $(document).on("click",".menuItem", function() {
+    var t = $(this);
     $(".menuItem").removeClass("selected");
-    $(this).addClass("selected");
+    t.addClass("selected");
+    $(".page").removeClass("selected");
+    $('#' + t.attr("data-target")).addClass("selected");
 });
